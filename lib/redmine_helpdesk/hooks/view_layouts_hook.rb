0b6d56ac2d4eef222f49418c931d360d2017d454
Maksim <max@koritskiy.ru>
2014-05-14 14:54:12 +0400

    fix layout hooks

--- lib/redmine_helpdesk/hooks/view_layouts_hook.rb
+++ lib/redmine_helpdesk/hooks/view_layouts_hook.rb
@@ -2,7 +2,7 @@ module RedmineHelpdesk
   module Hooks
     class ViewsLayoutsHook < Redmine::Hook::ViewListener
       def view_layouts_base_html_head(context={})
-        return stylesheet_link_tag(:helpdesk, :plugin => 'redmine_contacts_helpdesk')
+        return stylesheet_link_tag(:helpdesk, :plugin => 'redmine_contacts_helpdesk') unless defined?(RedminePluginAssetPipeline)
       end
     end
   end
